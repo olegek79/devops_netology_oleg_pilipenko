913a223a47d36bac44e550b84fdb2b2c72e68c3c
Oleg Pilipenko <Olegek.Pilipenko@yandex.ru>
2021-11-02 16:09:13 +0300

    merge: @ instead *

--- branching/merge.sh
+++ branching/merge.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # display command line options
+
 count=1
-for param in "$*"; do
-    echo "\$* Parameter #$count = $param"
+for param in "$@"; do
+    echo "\$@ Parameter #$count = $param"
     count=$(( $count + 1 ))
 done
-# изменения файла merge.sh
+
